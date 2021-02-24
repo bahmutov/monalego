ba8c79f17fe95936e1126efe4743218df5645372
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-24 17:50:36 -0500

    spec is working

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MonaLego
 
 This [Legra.js](https://legrajs.com/) demo copied from from [legra-monalisa.glitch.me/](https://legra-monalisa.glitch.me/)
+
+![MonaLego](./images/canvas-6.png)
+
+The test in [cypress/integration/spec.js](./cypress/integration/spec.js) changes the brick size and saves the produced Lego image as a local file.
---
+++ cypress/integration/spec.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+const downloadByClicking = (blob, name) => {
+  console.log('downloading image', name)
+  // blob ready, download it
+  let link = document.createElement('a')
+  link.download = name
+
+  link.href = URL.createObjectURL(blob)
+  link.click()
+
+  // delete the internal blob reference, to let the browser clear memory from it
+  URL.revokeObjectURL(link.href)
+}
+
+const downloadViaDataUrl = (brickValue) => {
+  const filename = `canvas-${brickValue}.png`
+  // the simplest way is to grab the data url and use
+  // https://on.cypress.io/writefile to save PNG file
+  cy.get('canvas').then(($canvas) => {
+    const url = $canvas[0].toDataURL()
+    const data = url.replace(/^data:image\/png;base64,/, '')
+    cy.writeFile(filename, data, 'base64')
+  })
+}
+
+describe('MonaLego', () => {
+  it('renders Lego', () => {
+    cy.visit('/')
+    // let's confirm min and max
+    cy.get('#range')
+      .should('have.attr', 'min')
+      .then(parseInt)
+      .should('be.lte', 10)
+    cy.get('#range')
+      .should('have.attr', 'max')
+      .then(parseInt)
+      .should('be.gte', 20)
+
+    cy.get('#range').invoke('val').then(downloadViaDataUrl)
+
+    // let's set the value
+    cy.get('#range').invoke('val', 6).trigger('input')
+    // how do we know when the Lego canvas has finished updating?
+    cy.wait(1000)
+    cy.get('#range').invoke('val').then(downloadViaDataUrl)
+  })
+})
