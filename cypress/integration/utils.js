c167d6acde3117402acd0531965ebec15b34dc4d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-01 15:25:09 -0500

    add images test

--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -3,7 +3,7 @@ export const downloadViaDataUrl = (name, brickValue) => {
 
   // the simplest way is to grab the data url and use
   // https://on.cypress.io/writefile to save PNG file
-  cy.get('canvas').then(($canvas) => {
+  return cy.get('canvas').then(($canvas) => {
     const url = $canvas[0].toDataURL()
     const data = url.replace(/^data:image\/png;base64,/, '')
     cy.writeFile(filename, data, 'base64')
