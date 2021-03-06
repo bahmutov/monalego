69956feff297f5637821e9feecfcad0227296ba0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-06 09:21:04 -0500

    add bar chart with visual tests

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 This [Legra.js](https://legrajs.com/) demo copied from from [legra-monalisa.glitch.me/](https://legra-monalisa.glitch.me/)
 
-![MonaLego](./images/canvas-6.png)
+![MonaLego](./images/darwin/canvas-20.png)
 
 The test in [cypress/integration/mona-spec.js](./cypress/integration/mona-spec.js) changes the brick size and saves the produced Lego image as a local file.
 
@@ -16,6 +16,12 @@ Related: [Fast legoization](https://glebbahmutov.com/blog/fast-legoization/)
 Read [Canvas Visual Testing with Retries](https://glebbahmutov.com/blog/canvas-testing/), see the test [cypress/integration/smile-spec.js](./cypress/integration/smile-spec.js).
 
 ![Visual canvas testing](./gif/recurse-smile.gif)
+
+## Bar chart
+
+An animated [Chart.js bar chart](https://www.chartjs.org/samples/latest/scriptable/bar.html) with multiple visual diffs against it. You can change the animation duration in [public/bar.html](./public/bar.html), the test should still work.
+
+![Visual tests against animated bar chart](./gif/bar.gif)
 
 [ci image]: https://github.com/bahmutov/monalego/workflows/main/badge.svg?branch=main
 [ci url]: https://github.com/bahmutov/monalego/actions
--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -1,13 +1,22 @@
-export const downloadPng = (filename) => {
-  expect(filename).to.be.a('string')
+// @ts-check
+/// <reference types="cypress" />
+import { recurse } from 'cypress-recurse'
+
+export const downloadPng = (filename, log = true) => {
+  if (typeof filename !== 'string') {
+    // trick: prints the argument to the assertion
+    expect(filename).to.be.a('string')
+  }
 
   // the simplest way is to grab the data url and use
   // https://on.cypress.io/writefile to save PNG file
-  return cy.get('canvas').then(($canvas) => {
+  return cy.get('canvas', { log }).then(($canvas) => {
     const url = $canvas[0].toDataURL()
     const data = url.replace(/^data:image\/png;base64,/, '')
-    cy.writeFile(filename, data, 'base64')
-    cy.wrap(filename)
+    // https://github.com/cypress-io/cypress/issues/15353
+    // @ts-ignore
+    cy.writeFile(filename, data, 'base64', { log })
+    cy.wrap(filename, { log })
   })
 }
 
@@ -41,4 +50,31 @@ export const downloadByClicking = (blob, name) => {
 
   // delete the internal blob reference, to let the browser clear memory from it
   URL.revokeObjectURL(link.href)
+}
+
+export const looksTheSame = (filename, log = true) => {
+  cy.log(`looks the same **${filename}**?`)
+  cy.then(() => {
+    if (typeof filename === 'string') {
+      expect(filename).to.be.a('string')
+    }
+
+    recurse(
+      () => {
+        return downloadPng(filename, log).then((filename) => {
+          if (log) {
+            cy.log(`saved ${filename}`)
+          }
+          return cy.task('compare', { filename }, { log })
+        })
+      },
+      ({ match }) => match,
+      {
+        log,
+      },
+    )
+    cy.then(() => {
+      cy.log(`**${filename}** looks the same`)
+    })
+  })
 }
