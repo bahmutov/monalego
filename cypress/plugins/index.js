61cdfc5829fd95ee25ebc8eceec4c3a2394266be
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-06 08:08:35 -0500

    add per-image diff name, closes #8

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,14 +10,21 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
   on('task', {
     async compare({ filename, options }) {
+      if (!filename.endsWith('.png')) {
+        const msg = `Expected image filename "${filename}" to end with .png`
+        console.error(msg)
+        throw new Error(msg)
+      }
       const baseFolder = 'images'
       const baseImage = path.join(baseFolder, path.basename(filename))
       const newImage = filename
-      const diffImage = 'diff.png'
+      const baseImageWithoutExtension = path.basename(filename, '.png')
+      const diffImage = `${baseImageWithoutExtension}-diff.png`
       console.log(
-        'comparing base image %s to the new image %s',
+        'comparing base image %s to the new image %s, diff image %s',
         baseImage,
         newImage,
+        diffImage,
       )
 
       const started = +new Date()
