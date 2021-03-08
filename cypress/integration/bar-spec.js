344094852b65fc9a328deaa946055b47a3a777ff
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-08 09:29:09 -0500

    split the test to get separate screenshots

--- cypress/integration/bar-spec.js
+++ cypress/integration/bar-spec.js
@@ -8,6 +8,32 @@ describe('Chartjs bar chart', () => {
     looksTheSame('bar-chart.png')
   })
 
+  // split the test to have a single image saved per test
+  // if the test fails on CI we can download an image for each test
+  it('adds a data set', () => {
+    const log = false
+
+    cy.visit('/bar')
+    looksTheSame('bar-chart.png', log)
+
+    cy.contains('button', 'Add Dataset').click()
+    looksTheSame('bar-chart-added-dataset.png', log)
+  })
+
+  it('adds two data sets', () => {
+    const log = false
+
+    cy.visit('/bar')
+    looksTheSame('bar-chart.png', log)
+
+    cy.contains('button', 'Add Dataset').click()
+    looksTheSame('bar-chart-added-dataset.png', log)
+
+    cy.contains('button', 'Add Dataset').click()
+    looksTheSame('bar-chart-3-sets.png', log)
+  })
+
+  // a single test can definitely perform multiple comparisons
   it('adds and removes data sets', () => {
     const log = false
 
